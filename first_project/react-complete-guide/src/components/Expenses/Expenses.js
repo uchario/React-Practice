40f91b76060d9621c2d12800c282b47ca9589e6d
uchario <uchario@yahoo.com>
2022-08-02 22:48:08 +0100

    modify Expenses.js & ExpenseItem.js

--- first_project/react-complete-guide/src/components/Expenses/Expenses.js
+++ first_project/react-complete-guide/src/components/Expenses/Expenses.js
@@ -1,9 +1,19 @@
+import React, {useState} from 'react';
+
 import ExpenseItem from './ExpenseItem';
 import Card from '../UI/Card';
+import ExpensesFilter from './ExpensesFilter';
+import './Expenses.css';
 
 const Expenses = (props) => {
+    const [filteredYear, setFilteredYear] = useState('2020');
+
+    const filterChangeHandler = (selectedYear) => {
+        setFilteredYear(selectedYear);
+    }
     return (
         <Card className='expenses'>
+            <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler}/>
             <ExpenseItem
                 title={props.expenses[0].title}
                 amount={props.expenses[0].amount}
